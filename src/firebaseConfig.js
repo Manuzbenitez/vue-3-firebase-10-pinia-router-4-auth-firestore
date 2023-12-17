1b10b8bed01dda8e18e3318adfa1de4c30d7fc8a
Emmanuel Benitez Ledesma <emmanuel.benitez@grupomarktel.com>
2023-12-17 22:29:34 +0100

    auth casi casi

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBO8MR6CqW3dNYlS5894tt5NM6SEWkNa7c",
+  authDomain: "vue-3-firebase2023.firebaseapp.com",
+  projectId: "vue-3-firebase2023",
+  storageBucket: "vue-3-firebase2023.appspot.com",
+  messagingSenderId: "670381094300",
+  appId: "1:670381094300:web:7f1205946ac49c6c004459"
+};
+
+initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export {auth}
